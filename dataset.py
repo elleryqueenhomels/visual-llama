01c56a36ea8e3b1150e1ee458507d5de47dcc54a
Wengao Ye <elleryqueenhomels@hotmail.com>
2023-06-22 00:40:47 +0100

    Add FinetuneDataset

--- dataset.py
+++ dataset.py
@@ -1,6 +1,8 @@
 import copy
 import json
+import yaml
 import random
+from PIL import Image
 
 import torch
 from torch.utils.data import Dataset
@@ -10,9 +12,9 @@ from llama import Tokenizer
 from llama import format_prompt
 
 
-def construct_sample(ann: dict, tokenizer: Tokenizer, max_words: int):
-    prompt = format_prompt(ann["instruction"], ann.get("input", ""))
-    example = prompt + ann["output"]
+def construct_sample(instruction: str, input: str, response: str, tokenizer: Tokenizer, max_words: int):
+    prompt = format_prompt(instruction, input)
+    example = prompt + response
     prompt = torch.tensor(tokenizer.encode(prompt, bos=True, eos=False), dtype=torch.int64)
     example = torch.tensor(tokenizer.encode(example, bos=True, eos=True), dtype=torch.int64)
     padding = max_words - example.shape[0]
@@ -46,7 +48,13 @@ class InstructionDataset(Dataset):
         return len(self.ann)
 
     def __getitem__(self, index):
-        return construct_sample(self.ann[index], self.tokenizer, self.max_words)
+        data_item = self.ann[index]
+        return construct_sample(
+            data_item['instruction'],
+            data_item['input'],
+            data_item['output'],
+            self.tokenizer,
+            self.max_words)
 
 
 class JointDataset(Dataset):
@@ -61,17 +69,71 @@ class JointDataset(Dataset):
         return len(self.cap_dataset) + len(self.inst_dataset)
     
     def __getitem__(self, index):
-        imgs = None
+        image = None
         tokens = None
         labels = None
         if index % 2 == 0:
             tokens, labels, _ = self.inst_dataset[index // 2]
-            imgs = torch.zeros(3, 224, 224)
+            image = torch.zeros(3, 224, 224)
         else:
-            imgs, targets = self.cap_dataset[index // 2]
-            imgs = self.img_transform_fn(imgs)
+            image, targets = self.cap_dataset[index // 2]
+            image = self.img_transform_fn(image)
             target = targets[random.randint(0, len(targets) - 1)]
-            ann = {"instruction": "Describe the image", "output": target}
-            tokens, labels, _ = construct_sample(ann, self.tokenizer, self.args.max_seq_len)
-        imgs = imgs.to(tokens.device)
-        return tokens, imgs, labels
+            ann = {"instruction": "Describe the image", "output": target, "input": ""}
+            tokens, labels, _ = construct_sample(
+                ann['instruction'],
+                ann['input'],
+                ann['output'],
+                self.tokenizer,
+                self.args.max_seq_len)
+        return tokens, image, labels
+
+
+class FinetuneDataset(Dataset):
+    def __init__(self, config_path, coco_dir, img_transform_fn, tokenizer: Tokenizer, max_words=30):
+        print(f"Reading dataset config from {config_path}")
+        with open(config_path, 'r') as f:
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
+        print("DATASET CONFIG:")
+        print(self.config)
+
+        ann = []
+        for meta_path in self.config['META']:
+            data = json.load(open(meta_path))
+            print(f"{meta_path}: {len(data)} records")
+            ann += data
+        self.ann = ann
+        print(f"total length: {len(self)}")
+
+        self.img_transform_fn = img_transform_fn
+        self.tokenizer = tokenizer
+        self.max_words = max_words
+        self.coco_dir = coco_dir
+
+    def __len__(self):
+        return len(self.ann)
+
+    def __getitem__(self, index):
+        data_item = self.ann[index]
+        if 'image' in data_item:
+            filename = data_item['image']
+            question = data_item['conversations'][0]['value']
+            answer = data_item['conversations'][1]['value']
+
+            image = Image.open(f"{self.coco_dir}/{filename}").convert('RGB')
+            image = self.img_transform_fn(image)
+            format_instruction = question
+            format_input = None
+        else:
+            image = torch.zeros(3, 224, 224)
+            format_instruction = data_item['instruction'],
+            format_input = data_item['input']
+            answer = data_item['output']
+
+        tokens, labels, _ = construct_sample(
+            format_instruction,
+            format_input,
+            answer,
+            self.tokenizer,
+            self.max_words)
+        return tokens, image, labels
